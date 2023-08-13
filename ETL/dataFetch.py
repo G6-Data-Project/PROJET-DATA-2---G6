fa24a2dad3c7c93211f6941315f822637d692cba
Mirado <hei.mirado@gmail.com>
2023-08-13 14:59:40 +0300

    add players from league 1 to 500 in csv file

--- ETL/dataFetch.py
+++ ETL/dataFetch.py
@@ -1,8 +1,8 @@
 import requests
 import csv
 
-start_league_id = 1
-end_league_id = 3
+start_league_id = 401
+end_league_id = 450
 apy_key = "ff59da0b57691fd4b8c7b19768cccbca0f2b1fba43a305c16c6e1071f4ea15f0"
 
 all_teams = []
@@ -15,7 +15,7 @@ for league_id in range(start_league_id, end_league_id + 1):
         teams = response.json()
         all_teams.extend(teams)
 
-    with open('players.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open('players_401_to_450.csv', 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = [
             'player_key', 'player_id', 'player_image', 'player_name', 'player_number',
             'player_country', 'player_type', 'player_age', 'player_match_played',
